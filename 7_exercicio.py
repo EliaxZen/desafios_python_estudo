86b7fbde9d56ea08bb729cb0eed045c2614becfa
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-02-10 08:26:17 -0300

    adicionando desafios

---
+++ 7_exercicio.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Desenvolva um script para encontrar um arquivo dentro da pasta home do usuário
+def encontra_arquivo(caminho, nome_do_arquivo):
+    for arquivo in caminho.glob('**/*'):
+        if arquivo.is_file():
+            if arquivo.stem == nome_do_arquivo:
+                print(arquivo)
+
+
+encontra_arquivo(Path.cwd(), 'arquivo2')
